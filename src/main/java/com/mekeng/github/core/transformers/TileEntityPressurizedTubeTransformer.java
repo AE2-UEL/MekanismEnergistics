48ea3107c223b65e3bd16333c51bf814ee3b5ea6
GlodBlock <1356392126@qq.com>
2024-06-03 00:41:50 +0800

    fix asm crash with mekceu

--- src/main/java/com/mekeng/github/core/transformers/TileEntityPressurizedTubeTransformer.java
+++ src/main/java/com/mekeng/github/core/transformers/TileEntityPressurizedTubeTransformer.java
@@ -26,8 +26,10 @@ public class TileEntityPressurizedTubeTransformer extends MkEClassTransformer.Cl
 
         @Override
         public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-            if (name.equals("update") || name.equals("doRestrictedTick")) {
+            if (name.equals("update") || name.equals("func_73660_a")) {
                 return new TransformUpdate(api, super.visitMethod(access, name, desc, signature, exceptions));
+            } else if (name.equals("doRestrictedTick")) {
+                return new TransformUpdateU(api, super.visitMethod(access, name, desc, signature, exceptions));
             }
             return super.visitMethod(access, name, desc, signature, exceptions);
         }
@@ -47,6 +49,37 @@ public class TileEntityPressurizedTubeTransformer extends MkEClassTransformer.Cl
             if (opcode == Opcodes.INVOKEINTERFACE && name.equals("drawGas")) {
                 if (ready) {
                     super.visitVarInsn(Opcodes.ALOAD, 5);
+                    super.visitMethodInsn(
+                            Opcodes.INVOKESTATIC,
+                            "com/mekeng/github/core/CoreHooks",
+                            "hooker$wrapGasHandler",
+                            "(Lmekanism/api/gas/IGasHandler;Lnet/minecraft/util/EnumFacing;IZLmekanism/api/gas/GasStack;)Lmekanism/api/gas/GasStack;",
+                            false
+                    );
+                    ready = false;
+                    return;
+                } else {
+                    ready = true;
+                }
+            }
+            super.visitMethodInsn(opcode, owner, name, desc, itf);
+        }
+
+    }
+
+    private static class TransformUpdateU extends MethodVisitor {
+
+        boolean ready = false;
+
+        TransformUpdateU(int api, MethodVisitor mv) {
+            super(api, mv);
+        }
+
+        @Override
+        public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+            if (opcode == Opcodes.INVOKEINTERFACE && name.equals("drawGas")) {
+                if (ready) {
+                    super.visitVarInsn(Opcodes.ALOAD, 6);
                     super.visitMethodInsn(
                             Opcodes.INVOKESTATIC,
                             "com/mekeng/github/core/CoreHooks",
